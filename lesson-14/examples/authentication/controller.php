12696428c71b396274a38d72aaac23ab7fe9f6c7
yujun-yeong <klmp83@naver.com>
2016-08-13 19:24:27 -0400

    TEST

--- lesson-14/examples/authentication/controller.php
+++ lesson-14/examples/authentication/controller.php
@@ -20,8 +20,8 @@
     echo "AUTHX <br />";
     
     try {
-    	if (function_exists("get_included_file_contents")) { // password_hash($password, PASSWORD_BCRYPT);
-    		echo "RESULT" . function_exists("get_included_file_contents") . "<br />";
+    	if (function_exists("session_start")) { // password_hash($password, PASSWORD_BCRYPT);
+    		echo "RESULT" . function_exists("session_start") . "<br />";
     	} else {
     		echo "NO FUNCTION <br />";
     		echo 'Current PHP version: ' . phpversion() . '.<br />';
