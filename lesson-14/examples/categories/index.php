ff0e943728202082eb31fa2830e7941e9a96fe9d
yujun-yeong <klmp83@naver.com>
2016-08-13 17:29:38 -0400

    TEST

--- lesson-14/examples/categories/index.php
+++ lesson-14/examples/categories/index.php
@@ -1,14 +1,14 @@
 <?php
 
   // require the controller
- require_once 'controller.php';
+  require_once 'controller.php';
 
   // require the HTML header
-//  require_once $_SERVER['DOCUMENT_ROOT'] . '/lesson-14/examples/includes/header.php';
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/lesson-14/examples/includes/header.php';
 
   // output
-//  echo $yield;
+  echo $yield;
   
 
   // require the HTML footer
-//  require_once $_SERVER['DOCUMENT_ROOT'] . '/lesson-14/examples/includes/footer.php';
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/lesson-14/examples/includes/footer.php';
